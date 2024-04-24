23749a7b920168614d247cb503a87011818adce6
julienp88 <165802750+lecerefe@users.noreply.github.com>
2024-04-24 16:01:42 +0200

    ajouts des cour 24/04

---
+++ 03-Algorithme/java/src/introduction/App.java
@@ -0,0 +1,42 @@
+package introduction;
+
+public class App {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("Hello World");   // Affiche "Hello World" dans la console
+	
+		//VARIABLES
+		
+		String prenom; // Déclaraition d'une variable "prenom" de type "chaine de caractères"
+		
+		int age; // Déclaration d'une variable "age" de type "Nombre entier" (valeur scalaire)
+		// (plage de valeurs d'un INT ( -2 147 000 000 à + 2 147 000 000)
+		
+		long soldbancaire; // // Déclaration d'une variable "soldeBancaire" de type "Nombre entier 64 bits"(valeur scalaire)	
+		
+		float tailleEnMetres;  // Déclaration d'une variable "tailleEnMetres" de type "Nombre réel 32 bits"(valeur scalaire)
+		
+		float tailleEnMetresPourLesTresGrands; // Déclaration d'une variable "tailleEnMetresPourLesTresGrnads" de type "Nombre réel 64 bits"(valeur scalaire)
+		
+		Boolean estMajeur; // Déclaration d'une variable "estMajeur" de type "Booléen"(valeur scalaire)
+	
+		prenom = "juju"; // Affectation d'une valeur à la variable "prenom"
+		
+		System.out.println(prenom);
+		
+		prenom = "lulu"; // Affectation d'une valeur à la variable "prenom" 
+		
+		System.out.println("Bonjour " + prenom);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
+
