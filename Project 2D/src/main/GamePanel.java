b6e0c60742885675d3bf45a6df281c403d899a09
julienp88 <165802750+lecerefe@users.noreply.github.com>
2024-04-30 16:56:13 +0200

    2 D game

---
+++ Project 2D/src/main/GamePanel.java
@@ -0,0 +1,96 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
+	
+	// SCREEN SETTINGS
+	final int originalTileSize = 16; // 16*16 tile
+	final int scale = 3;
+	
+	final int tileSize = originalTileSize * scale; // 48*48 tile
+	final int maxScreenCol = 16;
+	final int maxScreenRow = 12;
+	final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+	final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+	
+	// FPS
+	int FPS = 60;
+	
+	KeyHandler keyH = new KeyHandler();
+	Thread gameThread;
+	
+	// Set player's default position
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4;
+	
+	
+	public GamePanel() {
+		
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
+	}
+
+	
+	public void startGameThread() {
+		
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	@Override
+	public void run() {
+		
+		double drawInterval = 1000000000/FPS;
+		
+		
+		while(gameThread != null) {
+			
+			update();
+
+			repaint();
+		}
+		
+	}
+	
+	
+	public void update() {
+		
+		if (keyH.upPressed == true){
+			playerY -= playerSpeed;
+		}
+		else if(keyH.downPressed == true) {
+			playerY += playerSpeed;
+		}
+		else if(keyH.leftPressed == true) {
+			playerX -= playerSpeed;
+		}
+		else if (keyH.rightPressed == true) {
+			playerX += playerSpeed;
+		}
+		
+		
+	}
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D)g;
+		
+		g2.setColor(Color.white);
+		
+		g2.fillRect(playerX, playerY, tileSize , tileSize);
+		
+		g2.dispose();
+	}
+
+}
